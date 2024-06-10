481c1c578beda25b6f67df872f891fc4a181e7fb
aeliyadevs <aeliyadevs@gmail.com>
2024-06-10 13:25:45 +0545

    make the 'minute hand' & 'hour hand' turn to respective correct angle

--- js/main.js
+++ js/main.js
@@ -23,5 +23,26 @@ function startSecond() {
   angle += 6;
   secondHand.style.transform = "rotate(" + angle + "deg) translateY(50px)";
 }
-
 setInterval(startSecond, 1000);
+
+var minuteHand = document.getElementById("minute-hand");
+var minuteAngle = 0;
+function minuteHandTick() {
+  if (minuteAngle === 360) {
+    minuteAngle = 0;
+  }
+  minuteAngle += 0.1;
+  minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
+}
+setInterval(minuteHandTick, 1000);
+
+var hourHand = document.getElementById("hour-hand");
+var hourAngle = 0;
+function hourHandTick() {
+  if (hourAngle === 360) {
+    hourAngle = 0;
+  }
+  hourAngle += 0.1;
+  hourHand.style.transform = "rotate(" + hourAngle + "deg)";
+}
+setInterval(hourHandTick, 12000);
