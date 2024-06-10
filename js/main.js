c77ca13bc832ab3b20d27b811298acd7234cba35
aeliyadevs <aeliyadevs@gmail.com>
2024-06-10 15:00:32 +0545

    clock: add clock second needle sound with sound on/off button

--- js/main.js
+++ js/main.js
@@ -1,48 +1,75 @@
-let hours = document.getElementById("hour-numbers");
-
-for (i = 0; i < 12; i++) {
-  var angle = i * 30;
-  hours.children[i].style.transform = "rotate(" + angle + "deg)";
-}
-
-let minutes = document.getElementById("minute-lines");
-// var minuteLine = document.createElement("div");
-// minuteLine.classList.add("minute-line");
-// minutes.appendChild(minuteLine);
-for (i = 0; i < 60; i++) {
-  var angle = i * 6;
-  minutes.children[i].style.transform = "rotate(" + angle + "deg";
-}
-
 var secondHand = document.getElementById("second-hand");
-var angle = 0;
-function startSecond() {
-  if (angle === 360) {
-    angle = 0;
+var secondAngle = 0;
+
+let playSound = false;
+let clockTick = new Audio("../sounds/clock-tick.mp3");
+let soundStatus = document.getElementById("sound-status");
+
+function secondHandTurn() {
+  if (playSound) {
+    clockTick.play();
   }
-  angle += 6;
-  secondHand.style.transform = "rotate(" + angle + "deg) translateY(50px)";
+
+  if (secondAngle === 360) {
+    secondAngle = 0;
+  }
+  secondAngle += 6;
+  secondHand.style.transform =
+    "rotate(" + secondAngle + "deg) translateY(50px)";
 }
-setInterval(startSecond, 1000);
+// setInterval(secondHandTurn, 1000);
 
 var minuteHand = document.getElementById("minute-hand");
 var minuteAngle = 0;
-function minuteHandTick() {
+function minuteHandTurn() {
   if (minuteAngle === 360) {
     minuteAngle = 0;
   }
   minuteAngle += 0.1;
   minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
 }
-setInterval(minuteHandTick, 1000);
+// setInterval(minuteHandTurn, 1000);
 
 var hourHand = document.getElementById("hour-hand");
 var hourAngle = 0;
-function hourHandTick() {
+function hourHandTurn() {
   if (hourAngle === 360) {
     hourAngle = 0;
   }
   hourAngle += 0.1;
   hourHand.style.transform = "rotate(" + hourAngle + "deg)";
 }
-setInterval(hourHandTick, 12000);
+// setInterval(hourHandTurn, 12000);
+
+function toggleSound() {
+  playSound = !playSound;
+  if (playSound) {
+    soundStatus.innerHTML = '<i class="fa-solid fa-volume-high unmute"></i>';
+  } else {
+    soundStatus.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+  }
+}
+function onDocumentLoad() {
+  let hours = document.getElementById("hour-numbers");
+
+  for (i = 0; i < 12; i++) {
+    var angle = i * 30;
+    hours.children[i].style.transform = "rotate(" + angle + "deg)";
+  }
+
+  let minutes = document.getElementById("minute-lines");
+  for (i = 0; i < 60; i++) {
+    var angle = i * 6;
+    minutes.children[i].style.transform = "rotate(" + angle + "deg";
+  }
+
+  // let soundBtn = document.getElementById("sound-btn");
+  // soundBtn.addEventListener("click", function () {
+  //   soundStatus.innerHTML = "On";
+  //   playSound = true;
+  // });
+
+  setInterval(secondHandTurn, 1000);
+  setInterval(minuteHandTurn, 1000);
+  setInterval(hourHandTurn, 12000);
+}
