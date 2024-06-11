a2384ba1d542d3e4ac6952d4fb628b0e73379317
aeliyadevs <aeliyadevs@gmail.com>
2024-06-11 12:32:56 +0545

    clock: replace the default 12 o'clock time static time with current/present time and improve needle's movement logic.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-var secondHand = document.getElementById("second-hand");
-var secondAngle = 0;
-
 let playSound = false;
 let clockTick = new Audio("../sounds/clock-tick.mp3");
+let soundBtn = document.getElementById("sound-btn");
 let soundStatus = document.getElementById("sound-status");
 
+var secondHand = document.getElementById("second-hand");
+var secondAngle = 0;
 function secondHandTurn() {
   if (playSound) {
     clockTick.play();
   }
 
-  if (secondAngle === 360) {
+  if (secondAngle >= 360) {
+    hourHandTurn();
     secondAngle = 0;
   }
   secondAngle += 6;
@@ -22,8 +23,8 @@ function secondHandTurn() {
 var minuteHand = document.getElementById("minute-hand");
 var minuteAngle = 0;
 function minuteHandTurn() {
-  if (minuteAngle === 360) {
-    minuteAngle = 0;
+  if (minuteAngle >= 360) {
+    minuteAngle -= 360;
   }
   minuteAngle += 0.1;
   minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
@@ -33,25 +34,38 @@ function minuteHandTurn() {
 var hourHand = document.getElementById("hour-hand");
 var hourAngle = 0;
 function hourHandTurn() {
-  if (hourAngle === 360) {
-    hourAngle = 0;
+  if (hourAngle >= 360) {
+    hourAngle -= 360;
   }
-  hourAngle += 0.1;
+  hourAngle += 0.5;
   hourHand.style.transform = "rotate(" + hourAngle + "deg)";
 }
 // setInterval(hourHandTurn, 12000);
 
+function setHands() {
+  let now = new Date();
+
+  hourAngle = now.getHours() * 30 + now.getMinutes() * 0.5;
+  hourHand.style.transform = "rotate(" + hourAngle + "deg)";
+  minuteAngle = now.getMinutes() * 6 + now.getSeconds() * 0.1;
+  minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
+  secondAngle = now.getSeconds() * 6;
+  secondHand.style.transform =
+    "rotate(" + secondAngle + "deg) translateY(50px)";
+}
+
 function toggleSound() {
   playSound = !playSound;
   if (playSound) {
-    soundStatus.innerHTML = '<i class="fa-solid fa-volume-high unmute"></i>';
+    soundStatus.innerHTML = '<i class="fa-solid fa-volume-high"></i>';
+    soundBtn.classList.add("on");
   } else {
     soundStatus.innerHTML = '<i class="fa-solid fa-volume-xmark"></i>';
+    soundBtn.classList.remove("on");
   }
 }
 function onDocumentLoad() {
   let hours = document.getElementById("hour-numbers");
-
   for (i = 0; i < 12; i++) {
     var angle = i * 30;
     hours.children[i].style.transform = "rotate(" + angle + "deg)";
@@ -63,6 +77,7 @@ function onDocumentLoad() {
     minutes.children[i].style.transform = "rotate(" + angle + "deg";
   }
 
+  setHands();
   // let soundBtn = document.getElementById("sound-btn");
   // soundBtn.addEventListener("click", function () {
   //   soundStatus.innerHTML = "On";
@@ -71,5 +86,5 @@ function onDocumentLoad() {
 
   setInterval(secondHandTurn, 1000);
   setInterval(minuteHandTurn, 1000);
-  setInterval(hourHandTurn, 12000);
+  // setInterval(hourHandTurn, 60000);
 }
